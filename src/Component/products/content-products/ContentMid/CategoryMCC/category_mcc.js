551fd896fd4988534596c838a3e6e5066c501dfa
anhtu-devfe <nguyenanhtu2204tb@gmail.com>
2022-04-10 20:39:29 +0700

    add modal and callapi and count number category

--- src/Component/products/content-products/ContentMid/CategoryMCC/category_mcc.js
+++ src/Component/products/content-products/ContentMid/CategoryMCC/category_mcc.js
@@ -1,29 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styles from "../../Content_products.module.scss"
+import { useDispatch } from 'react-redux';
 import { HiChevronDown, HiOutlinePlusCircle, HiOutlineMinusCircle } from "react-icons/hi";
-export default function category_mcc({data}) {
+import { click_on_data } from '../../../../../Redux/Reducers/view_item';
+import Product_item from "./Product_MCC/product_MCC"
+export default function category_MCC({data,setIsModalVisible}) {
+  const dispatch=useDispatch
+  // const [count,setCount]=useState([])
     function categoryMCC(){
         let result=null;
         if(data.length>0){
-          result = data.map(item=>{
-          if(item.category==="MCC"){
+            result=data.map( (item,index)=>{
+            if(item.category==="MCC"){
               return(
-                <div className={styles.category_item}>
-                <div className={styles.img}>
-                  <img src={item.url} alt=""/>
-                </div>
-                <div className={styles.item_title}>
-                           {item.name}
-                </div>
-                <div className={styles.cost}>
-                  <span>{item.cost}</span>
-                  <i><HiOutlinePlusCircle/></i>
-                </div>
-              </div>
+               <Product_item key={index} product={item} setIsModalVisible={setIsModalVisible}/>
               )
-          }
-          })
-          return result;
+            }
+            })
+            return result
         }
       }
   return (

ae3556bf1bc7c905371a6cafe0a896bcf0bde4b2
anhtu-devfe <nguyenanhtu2204tb@gmail.com>
2022-07-07 00:02:09 +0700

    make ui home_page

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Banner from './Banner/Banner'
 import ListProducts from './ListProducts/listProducts'
 import SubBanner from './SubBanner/SubBanner'
+import TopRating from './TopRating/TopRating'
 export default function Home() {
     return (
         <>
             <Banner />
             <ListProducts />
             <SubBanner />
+            <TopRating />
         </>
     )
 }
